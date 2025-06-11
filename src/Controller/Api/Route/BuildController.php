67aa341d0d1f712874720fdfb1c149c8dff5aac5
ashse <ash.seva@gmail.com>
2025-06-11 12:26:13 +0300

    :lipstick: cosmetic

--- src/Controller/Api/Route/BuildController.php
+++ src/Controller/Api/Route/BuildController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[AsController]
 class BuildController extends AbstractController
--- src/Controller/Api/Route/Request/BuildRequest.php
+++ src/Controller/Api/Route/Request/BuildRequest.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller\Api\Route\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class BuildRequest
 {
     public function __construct(
+        #[Assert\NotBlank]
         public int $startId,
+
+        #[Assert\NotBlank]
         public int $endId,
     )
     {}
