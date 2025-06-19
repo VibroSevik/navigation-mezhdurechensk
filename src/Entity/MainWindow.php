84c8bf90698ed215683290a02d7155c77e2aad6d
ashse <ash.seva@gmail.com>
2025-06-19 12:04:14 +0300

    :link: integration between MainWindow entity and vich/uploader-bundle

--- src/Entity/MainWindow.php
+++ src/Entity/MainWindow.php
@@ -3,15 +3,30 @@
 namespace App\Entity;
 
 use App\Repository\MainWindowRepository;
+use App\Entity\Traits\CreatedAtTrait;
+use App\Entity\Traits\UpdatedAtTrait;
+use DateTime;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\HasLifecycleCallbacks]
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: MainWindowRepository::class)]
 class MainWindow
 {
+    use createdAtTrait;
+    use updatedAtTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[Vich\UploadableField(mapping: 'main_window_media', fileNameProperty: 'media')]
+    #[Assert\File(extensions: ['mp4', 'webm'])]
+    private ?File $mediaFile = null;
 
     #[ORM\Column(length: 255)]
     private ?string $media = null;
@@ -29,6 +44,21 @@ class MainWindow
     public function setMedia(string $media): static
     {
         $this->media = $media;
+
+        return $this;
+    }
+
+    public function getMediaFile(): ?File
+    {
+        return $this->mediaFile;
+    }
+
+    public function setMediaFile(?File $mediaFile): self
+    {
+        $this->mediaFile = $mediaFile;
+        if (null !== $mediaFile) {
+            $this->updatedAt = new DateTime();
+        }
 
         return $this;
     }
